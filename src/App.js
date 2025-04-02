996845a8c52798364228d53eb3a498e2ffe5091e
yiseul kim <kdew715@naver.com>
2025-04-02 16:22:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Box from "./component/Box";
 
+const choice = {
+  Splash: {
+    name: "Splash",
+    img: "https://png.pngtree.com/background/20230412/original/pngtree-water-bubbles-sea-background-picture-image_2398416.jpg",
+  },
+  Burn: {
+    name: "Burn",
+    img: "https://png.pngtree.com/png-clipart/20220321/original/pngtree-fire-png-or-psd-hd-image-png-image_7464348.png",
+  },
+  Grow: {
+    name: "Grow",
+    img: "https://cdn.crowdpic.net/detail-thumb/thumb_d_BD812B9CA5053678165CECD17C48060B.jpg",
+  },
+};
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const [showModal, setShowModal] = useState(true);
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); 
+    console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    console.log("random value", randomItem);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+  const judgement = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "Splash")
+      return computer.name === "Burn" ? "win" : "lose";
+    else if (user.name === "Burn")
+      return computer.name === "Grow" ? "win" : "lose";
+    else if (user.name === "Grow")
+      return computer.name === "Splash" ? "win" : "lose";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showModal && (
+            <div className="modal">
+              <div className="modal-content">
+                <p>아래 " 물 💧 , 불 🔥 , 나무 🌿 " 버튼을 눌러서 결투를 시작하세요.</p>
+                <button onClick={() => setShowModal(false)}>확인</button>
+              </div>
+            </div>
+          )}
+
+      <div className="main">
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
+      </div>
+      <div className="main">
+        <button onClick={() => play("Burn")}>불 🔥</button>
+        <button onClick={() => play("Splash")}>물 💧</button>
+        <button onClick={() => play("Grow")}>나무 🌿</button>
+      </div>
     </div>
   );
 }
---
+++ src/component/Box.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Box = (props) => {
+  let result;
+  if (
+    props.title === "Computer" &&
+    props.result !== "tie" &&
+    props.result !== ""
+  ) {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+  if (props.title === "Computer") {
+    console.log("computer", result);
+  }
+
+  return (
+    <div className={`box ${result}`}>
+      <h1>{props.title}</h1>
+      <h2 data-testid="item-name">{props.item && props.item.name}</h2>
+      <img className="item-img" src={props.item && props.item.img} />
+      <h2>{result}</h2>
+    </div>
+  );
+};
+
+export default Box;
